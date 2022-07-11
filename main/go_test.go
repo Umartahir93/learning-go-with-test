b0821369f652bb2bdc24ddf88edb3b555562c211
umartahir93 <chaudhuryumar.tahir@gmail.com>
2022-07-11 12:19:11 +0200

    Refactored Hello World (TDD)

--- main/go_test.go
+++ main/go_test.go
@@ -5,22 +5,25 @@ import (
 )
 
 func TestHello(t *testing.T) {
-	t.Run("saying hello to the people", func(t *testing.T) {
-		got := Hello("Umar")
-		want := "Hello, Umar"
+	autoCorrectMessage := func(t testing.TB, got, want string) {
+		t.Helper()
 
 		if got != want {
 			t.Errorf("got %q want %q", got, want)
 		}
+	}
+
+	t.Run("saying hello to the people", func(t *testing.T) {
+		got := Hello("Umar")
+		want := "Hello, Umar"
+		autoCorrectMessage(t, got, want)
+
 	})
 
 	t.Run("say Hello, World when an empty string is supplied", func(t *testing.T) {
 		got := Hello("")
 		want := "Hello, World"
-
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		autoCorrectMessage(t, got, want)
 
 	})
 
